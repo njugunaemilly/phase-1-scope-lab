9fed8d9170f1bf30a7893b1cf4f4fca2767ee980
Emilly Wanjiku <emilly.wanjiku@student.moringaschool.com>
2022-11-27 10:23:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase()
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer ='not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'mary';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'probably mary';
+
+    function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer = 'not mary'
+    }
+}
